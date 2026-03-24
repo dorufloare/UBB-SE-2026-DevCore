8f4f47e4b368cbd12aa6c63b0afc381d58e7b70a
hidanvlad <vlad.hidan@stud.ubbcluj.ro>
2026-03-24 17:09:22 +0200

    task 5,6,7 done

---
+++ DevCoreHospital/DevCoreHospital/Data/MedicalDataService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using DevCoreHospital.Models;
+using System.Linq;
+
+namespace DevCoreHospital.Data
+{
+    public class MedicalDataService
+    {
+        // Our "In-Memory" Table
+        private static List<MedicalEvaluation> _mockTable = new List<MedicalEvaluation>();
+
+        // TASK 5: Save to Table
+        public void SaveEvaluation(MedicalEvaluation record)
+        {
+            _mockTable.Add(record);
+        }
+
+        // TASK 6: SQL SELECT Query logic
+        public List<MedicalEvaluation> GetEvaluationsByDoctor(string doctorId)
+        {
+            
+            return _mockTable.Where(e => e.Evaluator != null && e.Evaluator.Id == doctorId).ToList();
+        }
+    }
+}
